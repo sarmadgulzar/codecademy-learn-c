26bcf3adc243f0419b5e4a12d1f2151b987c735b
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-08-20 02:45:28 +0500

    Added initialization

---
+++ 02-Variables/04-initialization.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int numOfBooks = 10;
+    char favLetter = 'A';
+    char favDigit = '7';
+    double costOfCandyBar = 1.99;
+
+    printf("Number of books: %d\n", numOfBooks);
+    printf("Your Favorite Letter is: %c\n", favLetter);
+    printf("Your Favorite Digit is: %c\n", favDigit);
+    printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
