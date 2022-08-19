064f120f562d80225b0981f45b6cb4a7427303bb
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-08-20 02:19:17 +0500

    Added syntax and errors

---
+++ 01-Hello-World/02-syntax-and-errors.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Origianl, uppercase M in Main() is a syntax error.
+// int Main()
+
+// Fixed, lowercase m in main() is correct.
+int main()
+{
+    // Origianl, semicolon is missing.
+    // printf("Hello World!\n")
+
+    // Fixed, semicolon is added.
+    printf("Hello World!\n");
+}
