7f68cd4cfb77885b3de239c28aeccfce73c3d340
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-08-20 02:28:32 +0500

    Added comments

---
+++ 01-Hello-World/04-comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Single line comment
+    printf("Chocolate chip cookies are way better than oatmeal raisin cookies.\n");
+
+    /* Multi line
+    comment */
+    printf("Learning the C programming language is an exciting adventure!\n");
+}
