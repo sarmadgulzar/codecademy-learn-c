45e541deece25954a4c6a74da8f461f0281b6c99
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-08-20 02:41:46 +0500

    Added printf

---
+++ 02-Variables/03-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int ageLearnedToRide = 5;
+
+    /*
+    SYMBOL      TYPE
+    %d or %i	int
+    %f	        double or float
+    %c	        char
+    */
+
+    printf("I was %i years old when I learned to ride a bike.\n", ageLearnedToRide);
+    printf("I hope I still remember how to ride.");
+}
