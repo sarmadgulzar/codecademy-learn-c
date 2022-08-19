afb096665a2fea29a4926b231d89c399bd8a9ea5
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-08-20 02:36:11 +0500

    Added name standards

---
+++ 02-Variables/01-naming-standards.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // Original
+    // int number !;
+    // int test value;
+    // int 1example;
+    // float int;
+
+    // Fixed
+    int number;
+    int test_value;
+    int example_1;
+    float fl;
+
+    printf("Hello World!\n");
+}
